a6e1eeb38377e3f7d24e626f1304474e91c2a0d9
dubovix <a.a.dubovik@gmail.com>
2024-01-13 00:02:16 +0300

    first commit

---
+++ taskmanager/main/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('about', views.about, name="about"),
+    path('create', views.create, name="create"),
+]
